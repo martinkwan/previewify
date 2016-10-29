b1fd223deac1bb06343c1dec568a53208f1e99a2
Martin <martinkkwan@gmail.com>
2016-10-29 02:54:10 -0700

    (feat) Add search artist feature

--- client/index.js
+++ client/index.js
@@ -6,7 +6,7 @@ $(function(){
   const theTemplate = Handlebars.compile(theTemplateScript);
 
   // Define Data
-  const context = {
+  let context = {
     artistName: 'Kaskade',
     artistImg: 'https://i.scdn.co/image/ebf5bef90cea454226547b616b4a2e9006fda189',
   };
@@ -17,3 +17,34 @@ $(function(){
   // Add compiled html to the page
   $('.content-placeholder').html(theCompiledHtml);
 });
+
+$('.search-form').submit((event) => {
+  event.preventDefault();
+  const artist = $('.search-form>input').val();
+  $.get('/artist', { artist }, (results) => {
+    let artistObj = JSON.parse(results).artists.items[0];
+    console.log(artistObj);
+    populateTemplate(artistObj);
+  })
+})
+
+function populateTemplate(artistObj) {
+
+  // Grab the template script
+  const theTemplateScript = $('#artist-profile-template').html();
+
+  // Compile the template
+  const theTemplate = Handlebars.compile(theTemplateScript);
+
+  // Define Data
+  let context = {
+    artistName: artistObj.name,
+    artistImg: artistObj.images[2].url,
+  };
+
+  // Pass data to template
+  let theCompiledHtml = theTemplate(context);
+
+  // Add compiled html to the page
+  $('.content-placeholder').html(theCompiledHtml);
+}
--- server.js
+++ server.js
@@ -10,6 +10,22 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/client/index.html'));
 });
 
+app.get('/artist', (req, res) => {
+  console.log(req.query.artist);
+  let url = `https://api.spotify.com/v1/search?query=${req.query.artist}&type=artist`;
+  request(url, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      console.log('Got something back from api to server');
+      // body = JSON.parse(body)
+      // console.log(body[0]);
+      res.end(body);
+    } else {
+      console.error();
+      res.end(body);
+    }
+  });
+});
+
 app.listen(port, function () {
   console.log('App is listening on port ' + port + '!');
 });
