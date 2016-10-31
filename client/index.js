c57f8a2aa26cb2eda37206df691fbfb0c45b17e9
Martin <martinkkwan@gmail.com>
2016-10-30 19:56:19 -0700

    (refactor) Clean up code for template functions

--- client/index.js
+++ client/index.js
@@ -1,51 +1,79 @@
 /**
  * TO DO : Modularize template function
+ * Change to promises
  */
 
-$(function(){
+$(() => {
   // Grab the template script
   const theTemplateScript = $('#artist-profile-template').html();
-
   // Compile the template
   const theTemplate = Handlebars.compile(theTemplateScript);
-
   // Define Data
-  let context = {
+  const context = {
     artistName: 'Kaskade',
     artistImg: 'https://i.scdn.co/image/ebf5bef90cea454226547b616b4a2e9006fda189',
   };
-
   // Pass data to template
-  let theCompiledHtml = theTemplate(context);
-
+  const theCompiledHtml = theTemplate(context);
   // Add compiled html to the page
-  $('.content-placeholder').html(theCompiledHtml);
+  $('.artist-profile-placeholder').html(theCompiledHtml);
 });
+
+function populateArtistProfile(artistObj) {
+  // Grab the template script
+  const theTemplateScript = $('#artist-profile-template').html();
+  // Compile the template
+  const theTemplate = Handlebars.compile(theTemplateScript);
+  // Pass data to template
+  const theCompiledHtml = theTemplate(artistObj);
+  // Add compiled html to the page
+  $('.artist-profile-placeholder').html(theCompiledHtml);
+}
+
+function populateTrackList(trackList) {
+  const theTemplateScript = $('#track-list-template').html();
+  const theTemplate = Handlebars.compile(theTemplateScript);
+  const theCompiledHtml = theTemplate(trackList);
+  $('.track-list-placeholder').html(theCompiledHtml);
+}
+
+function populateAlbumImg(albumObj){
+  const theTemplateScript = $('#album-list-template').html();
+  const theTemplate = Handlebars.compile(theTemplateScript);
+  const theCompiledHtml = theTemplate(albumObj);
+  $('.album-list-placeholder').html(theCompiledHtml);
+}
+
 
 $('.search-form').submit((event) => {
   event.preventDefault();
   const artist = $('.search-form>input').val();
   $.get('/artist', { artist }, (artistResults) => {
     let artistObj = JSON.parse(artistResults).artists.items[0];
-    populateTemplate(artistObj);
-    $.get('/tracks', { artistId: artistObj.id }, (trackResults) => {
+    artistObj = {
+      artistName: artistObj.name,
+      artistImg: artistObj.images[2].url,
+      artistId: artistObj.id,
+    };
+    populateArtistProfile(artistObj);
+    $.get('/tracks', { artistId: artistObj.artistId }, (trackResults) => {
       const trackList = JSON.parse(trackResults).tracks.map(track => track.name);
-      populateTrackList(trackList);
-      console.log(trackList);
-      $.get('/albums', { artistId: artistObj.id }, (albumResults) => {
+      populateTrackList({ trackList });
+      $.get('/albums', { artistId: artistObj.artistId }, (albumResults) => {
         const albumObj = JSON.parse(albumResults).items.map((album) => {
           return { albumImg: album.images[1].url, albumId: album.id };
         });
-        populateAlbumImg(albumObj);
-        console.log(albumObj);
+        populateAlbumImg({ albumObj });
       });
     });
   });
 });
+
 /**
  * Changes tracklist when an album art is clicked
+ * Cannot be an arrow function because of the 'this' binding
  */
-$('.album-list-placeholder').on('click', 'img', function (event) {
+$('.album-list-placeholder').on('click', 'img', function () {
   const albumId = $(this).data('album-id');
   $.get('/albumTracks', { albumId }, (albumTrackResults) => {
     // console.log(albumTrackResults);
@@ -53,62 +81,3 @@ $('.album-list-placeholder').on('click', 'img', function (event) {
     populateTrackList(trackList);
   });
 });
-
-function populateTemplate(artistObj) {
-
-  // Grab the template script
-  const theTemplateScript = $('#artist-profile-template').html();
-
-  // Compile the template
-  const theTemplate = Handlebars.compile(theTemplateScript);
-
-  // Define Data
-  let context = {
-    artistName: artistObj.name,
-    artistImg: artistObj.images[2].url,
-  };
-
-  // Pass data to template
-  let theCompiledHtml = theTemplate(context);
-
-  // Add compiled html to the page
-  $('.content-placeholder').html(theCompiledHtml);
-}
-
-function populateTrackList(trackList) {
-  // Grab the template script
-  const theTemplateScript = $('#track-list-template').html();
-
-  // Compile the template
-  const theTemplate = Handlebars.compile(theTemplateScript);
-
-  // Define Data
-  let context = {
-    tracks: trackList,
-  };
-
-  // Pass data to template
-  let theCompiledHtml = theTemplate(context);
-
-  // Add compiled html to the page
-  $('.track-list-placeholder').html(theCompiledHtml);
-}
-
-function populateAlbumImg(albumObj){
-  // Grab the template script
-  const theTemplateScript = $('#album-list-template').html();
-
-  // Compile the template
-  const theTemplate = Handlebars.compile(theTemplateScript);
-
-  // Define Data
-  let context = {
-    albumObj,
-  };
-
-  // Pass data to template
-  let theCompiledHtml = theTemplate(context);
-
-  // Add compiled html to the page
-  $('.album-list-placeholder').html(theCompiledHtml);
-}
