bb0d51dbe0de875fd6d236a7c8f2d599cbb2d2dc
Martin <martinkkwan@gmail.com>
2016-12-08 12:11:35 -0800

    (feat) Add cssmin to gruntfile with sourcemap option

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,25 +6,49 @@ module.exports = (grunt) => {
     sass: {
       dist: {
         options: {
-          sourcemap: 'none',
+          sourceMap: 'none',
         },
         files: {
           'client/css/main.css': 'client/sass/main.scss',
         },
       },
     },
+    cssmin: { // Begin CSS Minify Plugin
+      options: {
+        sourceMap: true,
+      },
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'client/css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'client/css',
+          ext: '.min.css',
+        }],
+      },
+    },
+    uglify: { // Begin JS Uglify Plugin
+      build: {
+        src: ['client/*.js'],
+        dest: 'script.min.js',
+      },
+    },
     watch: {
       css: {
         files: '**/*.scss',
-        tasks: ['sass'],
+        tasks: ['sass', 'cssmin'],
+      },
+      js: {
+        files: '**/*.js',
+        tasks: ['uglify'],
       },
     },
   });
   // Load Grunt plugins
   grunt.loadNpmTasks('grunt-contrib-sass');
   // grunt.loadNpmTasks('grunt-postcss');
-  // grunt.loadNpmTasks('grunt-contrib-cssmin');
-  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Register Grunt tasks
